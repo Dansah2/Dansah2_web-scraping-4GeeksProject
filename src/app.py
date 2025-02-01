9a5cb026a58724b7c3c6ecd25774b4bc23b895cd
Dyimah Ansah <hdyimah@yahoo.com>
2025-02-01 01:12:22 +0000

    saved the data in a DataFrame

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import time
 import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 # select the webpage url to scrape data from
 tesla_url = "https://companies-market-cap-copy.vercel.app/index.html"
@@ -18,4 +19,22 @@ if response.status_code == 200:
 else:
     print("Error, the web content was not receeived")
 
-print(web_content)
+#print(web_content.prettify())
+
+# Find the table with the year evolution.
+table = web_content.find("table")
+#print(table)
+
+# Obtain the data and its rows skipping the header row
+rows = table.find_all("tr")[1:]
+
+# Store the data in a DataFrame.
+data = [[col.text.strip() for col in row.find_all("td")] for row in rows]
+# print(data)
+
+# create dataframe
+data_df = pd.DataFrame(data, columns=['Date', 'Revenue', 'Percentage Change'])
+
+# sort the data by date in descending order
+data_df = data_df.sort_values('Date', ascending=False)
+print(data_df)
