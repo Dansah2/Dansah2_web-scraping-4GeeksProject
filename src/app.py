353ae3262aa500d3ed64f32bf7381cf258c49aa0
Dyimah Ansah <hdyimah@yahoo.com>
2025-02-01 00:23:59 +0000

    initial commit

--- src/app.py
+++ src/app.py
@@ -6,3 +6,16 @@ import sqlite3
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# select the webpage url to scrape data from
+tesla_url = "https://companies-market-cap-copy.vercel.app/index.html"
+
+# request to download fht efile from the internet
+response = requests.get(tesla_url)
+
+# if the request was opened correctly then store the data in a csv file
+if response.status_code == 200:
+    web_content = BeautifulSoup(response.text, 'html.parser')
+else:
+    print("Error, the web content was not receeived")
+
+print(web_content)
